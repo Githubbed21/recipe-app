3fb7f0a40888e7bbc4d732addd6098543cce0589
dean <diazdean21@gmail.com>
2022-03-05 22:24:32 -0600

    made my dropdown search work corectly

--- src/Recipes.js
+++ src/Recipes.js
@@ -6,13 +6,17 @@ const initialFormValues = {
 };
 
 export default function Recipes() {
-  const [recipes, setRecipes] = useState();
+  const [recipes, setRecipes] = useState([]);
+  const [displayedRecipes, setDisplayedRecipes] = useState([]);
   const [formValues, setFormValues] = useState(initialFormValues);
 
   useEffect(() => {
     fetch('http://localhost:3001/Recipes')
       .then((res) => res.json())
-      .then((data) => setRecipes(data))
+      .then((data) => {
+        setRecipes(data);
+        setDisplayedRecipes(data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -22,7 +26,16 @@ export default function Recipes() {
 
   const filter = (e) => {
     e.preventDefault();
+    const { title, meal } = formValues;
     console.log(formValues);
+
+    if (meal === 'All') {
+      setDisplayedRecipes(recipes);
+      return;
+    }
+
+    const filtered = recipes.filter((recipe) => recipe.meal === meal);
+    setDisplayedRecipes(filtered);
   };
 
   if (!recipes) {
@@ -46,10 +59,11 @@ export default function Recipes() {
           <option value="Breakfast">Breakfast</option>
           <option value="Lunch">Lunch</option>
           <option value="Dinner">Dinner</option>
+          <option value="Dessert">Dessert</option>
         </select>
         <input type="submit" value="Search" />
       </form>
-      {recipes.map((recipe) => {
+      {displayedRecipes.map((recipe) => {
         return (
           <div>
             <h3>{recipe.title}</h3>
